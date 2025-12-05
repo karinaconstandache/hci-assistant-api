14d07e0721e609fde93eb66a9b1887d29fa31736
Karina-Melissa Constandache <karinaconstandache12@gmail.com>
2025-12-05 11:05:39 +0200

    lab10

--- src/HCI.AiAssistant.API/Services/AppConfigurationsService.cs
+++ src/HCI.AiAssistant.API/Services/AppConfigurationsService.cs
@@ -6,4 +6,5 @@ public class AppConfigurationsService : IAppConfigurationsService
     public string? KeyVaultName { get; set; }
     public string? SecretsPrefix { get; set; }
     public string? IoTDeviceName { get; set; }
+    public string? Instruction { get; set; }
 }
--- src/HCI.AiAssistant.API/Services/IAppConfigurationsService.cs
+++ src/HCI.AiAssistant.API/Services/IAppConfigurationsService.cs
@@ -6,4 +6,5 @@ public interface IAppConfigurationsService
     public string? KeyVaultName { get; set; }
     public string? SecretsPrefix { get; set; }
     public string? IoTDeviceName { get; set; }
+    public string? Instruction { get; set; }
 }
